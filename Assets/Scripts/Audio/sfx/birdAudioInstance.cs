faed4971b2a9172abae2324e7a6bb4cab1fc4805
Colbygrimmscott <colbygrimmscott@ufl.edu>
2026-07-30 15:22:00 -0400

    Added scripting for bird and frog audio

---
+++ Assets/Scripts/Audio/sfx/birdAudioInstance.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using FMODUnity;
+
+namespace Game.Audio.Environment
+{
+    public class birdAudioInstance : MonoBehaviour
+    {
+        // Serialized field to get bird event
+        [Header("Bird SFX Event")]
+        [SerializeField] private EventReference birdEventReference;
+
+        private PersistentAudioInstance birdAudioWrapper;
+
+
+        //Creates a new bird event instance
+        private void Awake()
+        {
+            birdAudioWrapper = new PersistentAudioInstance(birdEventReference, transform);
+        }
+
+        // Plays bird sound
+        private void Start()
+        {
+            birdAudioWrapper.PlayAudio();
+        }
+
+                // For debug, draws gizmos for wind spawn zones and wind sound itself
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.cyan;
+            Gizmos.DrawWireSphere(transform.position, 4);
+        }
+    }
+}
